cf239da13f2c050e4d2984bf7727f72995660cb7
crystel1996 <crystelratsimbazafy@gmail.com>
2024-07-04 13:14:30 +0300

    add migrations

--- backend/migrations/Version20240704101306.php
+++ backend/migrations/Version20240704101306.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20240629111301 extends AbstractMigration
+final class Version20240704101306 extends AbstractMigration
 {
     public function getDescription(): string
     {
@@ -20,15 +20,13 @@ final class Version20240629111301 extends AbstractMigration
     public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('DROP SEQUENCE team_id_seq CASCADE');
-        $this->addSql('DROP SEQUENCE player_id_seq CASCADE');
         $this->addSql('CREATE TABLE player (id UUID NOT NULL, id_team_id UUID NOT NULL, first_name VARCHAR(255) NOT NULL, last_name VARCHAR(255) NOT NULL, balance DOUBLE PRECISION NOT NULL, PRIMARY KEY(id))');
         $this->addSql('CREATE INDEX IDX_98197A65F7F171DE ON player (id_team_id)');
         $this->addSql('COMMENT ON COLUMN player.id IS \'(DC2Type:uuid)\'');
         $this->addSql('COMMENT ON COLUMN player.id_team_id IS \'(DC2Type:uuid)\'');
         $this->addSql('CREATE TABLE team (id UUID NOT NULL, name VARCHAR(255) NOT NULL, country VARCHAR(255) NOT NULL, balance DOUBLE PRECISION NOT NULL, PRIMARY KEY(id))');
         $this->addSql('COMMENT ON COLUMN team.id IS \'(DC2Type:uuid)\'');
-        $this->addSql('CREATE TABLE "user" (id UUID NOT NULL, email VARCHAR(255) NOT NULL, password VARCHAR(255) NOT NULL, PRIMARY KEY(id))');
+        $this->addSql('CREATE TABLE "user" (id UUID NOT NULL, email VARCHAR(255) NOT NULL, username VARCHAR(255) NOT NULL, password VARCHAR(255) NOT NULL, PRIMARY KEY(id))');
         $this->addSql('COMMENT ON COLUMN "user".id IS \'(DC2Type:uuid)\'');
         $this->addSql('ALTER TABLE player ADD CONSTRAINT FK_98197A65F7F171DE FOREIGN KEY (id_team_id) REFERENCES team (id) NOT DEFERRABLE INITIALLY IMMEDIATE');
     }
@@ -37,8 +35,6 @@ final class Version20240629111301 extends AbstractMigration
     {
         // this down() migration is auto-generated, please modify it to your needs
         $this->addSql('CREATE SCHEMA public');
-        $this->addSql('CREATE SEQUENCE team_id_seq INCREMENT BY 1 MINVALUE 1 START 1');
-        $this->addSql('CREATE SEQUENCE player_id_seq INCREMENT BY 1 MINVALUE 1 START 1');
         $this->addSql('ALTER TABLE player DROP CONSTRAINT FK_98197A65F7F171DE');
         $this->addSql('DROP TABLE player');
         $this->addSql('DROP TABLE team');
